217fcab08f460f95f5713f5c9157dd942e5a938c
AufarR <aufaralazhar@gmail.com>
2023-12-07 16:14:20 +0700

    Enable db indexing

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,10 +1,5 @@
 const mongoose = require("mongoose");
 
 exports.connect = (uri) => {
-  const options = {
-    autoIndex: false, // Don't build indexes
-      maxPoolSize: 10, // Maintain up to 10 socket connections
-  };
-
-  mongoose.connect(uri, options)
+  mongoose.connect(uri)
 };
