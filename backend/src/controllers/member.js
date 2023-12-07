22b61fca940065640119e10203dd7f47d446e5c5
AufarR <aufaralazhar@gmail.com>
2023-12-07 16:14:55 +0700

    Finish controller

--- backend/src/controllers/member.js
+++ backend/src/controllers/member.js
@@ -1,17 +1,44 @@
 const Member = require("../models/member");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const login = async (req,res) => {
-    res.send()
+    try {
+        const {email, password} = req.body
+
+        const user = await Member.findOne({email})
+        if (!user || !email || !password) throw new Error("Invalid login")
+
+        if (!bcrypt.compareSync(password, user.password)) throw new Error("Invalid login")
+
+        const token = jwt.sign({name: user.name, email: user.email},process.env.JWT_SECRET,{ expiresIn: '10d' })
+
+        res.status(200).json({name: user.name, email: user.email, phone: user.phone, token})
+    } catch (e) {
+        console.error(e);
+        e.message === "Invalid login" ? res.status(400).json({message: e.message}) : res.status(500).send()
+    }
 }
 
 const register = async (req,res) => {
     try {
-        const {name, email, password, phone} = req.body
+        const {name, email, phone} = req.body
+        password = bcrypt.hashSync(req.body.password)
         const newMember = new Member({name, email, password, phone})
         await newMember.save()
-        res.status(201).json(newMember)
+        res.status(201).json({
+            _id: newMember._id,
+            name,
+            email,
+            phone,
+            createdAt: newMember.createdAt,
+        })
+    
     } catch (e) {
+        if (e.name === "ValidationError") return res.status(400).json({message: e.message})
+        if (e.message.includes("E11000")) return res.status(400).json({message: 'Duplicate email address'})
         console.error(e)
         res.status(500).send()
     }
