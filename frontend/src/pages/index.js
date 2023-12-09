c3556e2fc3c207e4c03279b7e2a8bd365f32addf
AufarR <aufaralazhar@gmail.com>
2023-12-09 14:38:22 +0700

    Update image

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,7 +1,7 @@
 import DefaultLayout from "@layouts/default";
 
 export default function Home() {
-  const imageUrl = 'https://www.bing.com/ck/a?!&&p=09f8794b29082f08JmltdHM9MTcwMTk5MzYwMCZpZ3VpZD0zMDZlYmNlOS02YTUyLTYxODYtMDdjNC1hZjZlNmIwNDYwM2YmaW5zaWQ9NTU5MQ&ptn=3&ver=2&hsh=3&fclid=306ebce9-6a52-6186-07c4-af6e6b04603f&u=a1L2ltYWdlcy9zZWFyY2g_cT1nbWJhciBjb3dvcmtpbmcgc3BhY2UmRk9STT1JUUZSQkEmaWQ9QUIwMzdDNDkxQjg1NTY5REU3RkNEMUVFNDIwNDNDM0EyQjAxOThGRg&ntb=1';
+  const imageUrl = 'https://spaceiq.com/wp-content/uploads/2020/11/Web_150DPI-20190305-WeWork-1330-Lagoon-Common-Areas-Wide-1-scaled.jpg';
     return (
       <div>
         <h1>Welcome To Coworking Space</h1>
